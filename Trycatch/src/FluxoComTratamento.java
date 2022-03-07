ac011adf5166b79eb9d8b3f10cc71a0bddf671af
MikeSSJ <74191867+MIkeSSJ@users.noreply.github.com>
2022-03-07 15:31:12 -0300

    Add files via upload

---
+++ Trycatch/src/FluxoComTratamento.java
@@ -0,0 +1,22 @@
+
+public class FluxoComTratamento {
+
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		metodo1();
+
+		System.out.println("Fim do main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		throw new MinhaExcecao("Deu muito erro");
+		
+		}
+}
